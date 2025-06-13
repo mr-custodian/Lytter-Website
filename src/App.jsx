434f7045d18c0aaf5121b4528e3c813f43930efa
Mayank <mayankspecial26@gmail.com>
2025-06-13 14:27:12 +0530

    Lytter Website Android Compatible

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Success from "./pages/Success";
-import Navbar from "./Components/Navbar";
+import NavigationBar from "./Components/Navbar";
 import Home from "./pages/Home";
 import Recycler from "./pages/Recycler";
 import Segregator from "./pages/Segregator";
@@ -12,12 +11,11 @@ import GetInTouch from "./pages/GetInTouch";
 function App() {
   return (
     <Router>
-      <Navbar/>
+      <NavigationBar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Recycler" element={<Recycler />} />
         <Route path="/Segregator" element={<Segregator />} />
-        <Route path="/success" element={<Success />} />
         <Route path="/Resources" element={<Resources />} />
         <Route path="/GetInTouch" element={<GetInTouch />} />
       </Routes>
